bc11f8224d9c4417eef463d23b0bd69060f6ab56
lalo2911 <luiseduardotorres-29@hotmail.com>
2025-04-01 17:25:44 -0600

    minor aesthetic changes

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 function RootLayout() {
     return (
         <div className="flex flex-col min-h-screen">
-            <Navbar />
             <main className="flex-grow container mx-auto px-4 py-8">
                 <Outlet /> {/* Aquí se renderizarán las páginas hijas */}
             </main>
